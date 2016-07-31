44f177f4ab85502ee2631dfcc2f7aa24219de10d
Abdulrahman <bydeinon@gmail.com>
2016-07-31 23:04:08 +0000

    Get MongoDB sports data

--- server/app.js
+++ server/app.js
@@ -1,15 +1,22 @@
 'use strict';
 
 let express = require('express');
-let app = express();
-
+let mongoUtil = require('./mongoUtil');
+mongoUtil.connect();
 const host = process.env.IP;
 const port = process.env.PORT;
+
+let app = express();
 
 app.use(express.static(__dirname + '/../client'));
 
 app.get('/sports', (request, response) => {
-	response.json(["Equestrian", "Swimming", "Track & Field"]);
+	let sports = mongoUtil.sports();
+	sports.find().toArray((err, docs) => {
+		console.log(JSON.stringify(docs));
+		let sportNames = docs.map((sport) => sport.name);
+		response.json(sportNames);
+	});
 });
 
 app.listen(port, () => console.log(`Server running at http://${host}:${port}/`));
