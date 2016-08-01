965469b37a73ea6fe2dff729ac7a7f8aad3081d9
Abdulrahman <bydeinon@gmail.com>
2016-08-01 21:04:20 +0000

    Add POST endpoint

--- server/app.js
+++ server/app.js
@@ -1,7 +1,9 @@
-'use strict';
+"use strict";
 
-let express = require('express');
-let mongoUtil = require('./mongoUtil');
+let express = require("express");
+let bodyParser = require("body-parser");
+let jsonParser = bodyParser.json();
+let mongoUtil = require("./mongoUtil");
 
 mongoUtil.connect();
 
@@ -10,9 +12,9 @@ const port = process.env.PORT;
 
 let app = express();
 
-app.use(express.static(__dirname + '/../client'));
+app.use(express.static(__dirname + "/../client"));
 
-app.get('/sports', (request, response) => {
+app.get("/sports", (request, response) => {
 	let sports = mongoUtil.sports();
 	sports.find().toArray((err, docs) => {
 		if(err) response.sendStatus(400);
@@ -22,7 +24,7 @@ app.get('/sports', (request, response) => {
 	});
 });
 
-app.get('/sports/:name', (request, response) => {
+app.get("/sports/:name", (request, response) => {
 	let sportName = request.params.name;
 	let sports = mongoUtil.sports();
 
@@ -31,6 +33,14 @@ app.get('/sports/:name', (request, response) => {
 		console.log("Sport doc: ", doc);
 		response.json(doc);
 	});
+});
+
+app.post("/sports/:name/medals", jsonParser, (request, response) => {
+	let sportName = request.params.name;
+	let newMedal = request.body.medal;
+	console.log("Sport name:",sportName)
+	console.log("Medal:",newMedal);
+	response.sendStatus(201);
 });
 
 app.listen(port, () => console.log(`Server running at http://${host}:${port}/`));
