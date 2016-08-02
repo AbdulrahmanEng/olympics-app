c8bee5414f91f847970c8c4c14478ee0ec09428a
Abdulrahman <bydeinon@gmail.com>
2016-08-02 10:16:33 +0000

    Add POST request on saveMedal() and RegExp match for tabs

--- client/src/app.js
+++ client/src/app.js
@@ -14,8 +14,17 @@ angular.module('olympics', ['ui.router'])
 				return $http.get('/sports');
 			}
 		},
-		controller: function (sportsService) {
+		controller: function (sportsService, $location) {
 			this.sports = sportsService.data;
+
+			this.isActive = function(sport){
+				let pathRegexp = /sports\/(\w+)/;
+				let match = pathRegexp.exec($location.path());
+				(match[1] === 'Track')?(match[1] = 'Track & Field'):false
+				if(match === null || match.length === 0) return false;
+				let selectedSportName = match[1];
+				return sport === selectedSportName;
+			};
 		},
 		controllerAs: 'sportsCtrl'
 	})
@@ -35,12 +44,16 @@ angular.module('olympics', ['ui.router'])
 	.state('sports.new', {
 		url: '/:sportName/medal/new',
 		templateUrl: 'sports/new-medal.html',
-		controller: function($stateParams, $state){
+		controller: function($stateParams, $state, $http){
 			this.sportName = $stateParams.sportName;
-			this.saveMedal = function(medal){		
-				console.log('medal:', medal);
-
-				$state.go('sports.medals', {sportName: $stateParams.sportName});
+			this.saveMedal = function(medal){			
+				$http({
+				method:'POST', 
+				url: `/sports/${$stateParams.sportName}/medals`, 
+				data:{medal}})
+				.then(function(){
+					$state.go('sports.medals', {sportName: $stateParams.sportName});
+				});
 			}
 		},
 		controllerAs: 'newMedalCtrl'
